be5eaddca538651e3f3f5ec3dfa6bb13e64251c8
dmc25 <dcurren@cas.org>
2022-07-05 10:57:07 -0400

    Initial Project generated by Spring intializer with SpringWeb

---
+++ src/main/java/com/manifestcorp/Techreads/TechReadsApplication.java
@@ -0,0 +1,13 @@
+package com.manifestcorp.Techreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechReadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechReadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manifestcorp/Techreads/TechReadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manifestcorp.Techreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechReadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
